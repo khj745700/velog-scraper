f068339888e831f1b6876c0f32434c799a0ddb13
KimHyunJin <khj745700@naver.com>
2025-02-13 23:32:52 +0900

    feat: puppeteer  launch args 추가

--- index.js
+++ index.js
@@ -33,9 +33,10 @@ async function initializeDatabase() {
 async function scrapeVelogTrending() {
     const browser = await puppeteer.launch({ 
         headless: true,
-        executablePath: '/usr/bin/chromium-browser'
+        executablePath: '/usr/bin/chromium-browser',
+        args: ['--no-sandbox', '--disable-dev-shm-usage']
      });
-     
+
     const page = await browser.newPage();
     await page.goto(VELOG_TREND_URL);
 
